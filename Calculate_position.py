f2e8f31ca6f8215f2bf30798fbdd7c2d6a5dbf94
Manurodo <manurodo@myuax.com>
2024-10-15 16:58:20 +0200

    Calculate position proyect

--- Calculate_position.py
+++ Calculate_position.py
@@ -1,12 +1,14 @@
 #Create a Python program that analyzes the motion of an object based on user input. 
 #The program will calculate the final position (x), velocity (v), and acceleration (a) of the object
-print ("En este programa se calculara la posicion de una particula suponiendo un movimiento uniformemente acelerado con actualizacion de 1 segundo")
-x0 = int(input ("introduzca la posicion en metros: "))
-v0 = int(input ("introduzca la velocidad en metros por segundo: "))
-a = int(input ("introduzca la aceleración en metros por segundo al cuadrado: "))
-t = 1
+print("En este programa se calculará la posición de una partícula suponiendo un movimiento uniformemente acelerado")
+x0 = float(input("Introduzca la posición inicial en metros: "))
+v0 = float(input("Introduzca la velocidad inicial en metros por segundo: "))
+a = float(input("Introduzca la aceleración en metros por segundo al cuadrado: "))
+t = float(input("Introduzca el tiempo en segundos: "))
+
 x = x0 + v0*t + (0.5*a*(t**2))
 v = v0 + a*t
-print("la posicion de la particula es", x, "m")
-print("la velocidad de la particula es", v, "m*s")
-print("la aceleracion es constante por lo que", a ,"sigue siendo la aceleración")
+
+print("La posición de la partícula tras", t, "s es:", x, "m")
+print("La velocidad de la partícula es", v, "m/s")
+print("La aceleración es constante, por lo que", a, "sigue siendo la aceleración")
