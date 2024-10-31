d53d4960a6c9fab30cab1ef61216c2acd41b340f
Manurodo <manuelrosales2005@outlook.es>
2024-10-31 16:10:31 +0100

    31/10

--- Informatica/Ejercicios/Bank_account.py
+++ Informatica/Ejercicios/Bank_account.py
@@ -4,19 +4,18 @@ class BankAccount:
         self.balance = 0
         
     def depositar(self, ammount):
+        ammount = float(ammount)
         self.balance = self.balance + ammount
-        return (f"el nuevo balance es {self.balance}€")
+        return (f"El nuevo balance es {self.balance}€")
 
     def retirar(self, ammount):
+        ammount = float (ammount)
         if self.balance >= ammount:
             self.balance = self.balance - ammount
-            return (f"el nuevo balance es {self.balance}€")
+            return (f"El nuevo balance es {self.balance}€")
 
         else:
             return ("no existe suficiente dinero en la cuenta")
     
     def cantidad(self):
-        return (f"el balance es: {self.balance}€")
-
-
-
+        return (f"El balance es: {self.balance}€")
--- Informatica/Ejercicios/ChatBankBot (CBB).py
+++ Informatica/Ejercicios/ChatBankBot (CBB).py
@@ -1,5 +1,5 @@
 from Bank_account import BankAccount
-from random import *
+from random import randint
 
 print ("Por favor introduzca su MyBank ID")
 print ("Si no tiene un MyBank ID introduzca su nombre y le proporcionaremos un MyBank ID nuevo")
@@ -12,31 +12,36 @@ except:
     ValueError
     name = in_
     ID = randint (11111, 99999)
-    print ("Buenas tardes",name,", soy su asistente personal del Banco MyManueBank y su nuevo ID es :", ID)
+    print ("Buenas tardes",name,"soy su asistente personal del Banco MyManueBank y su nuevo ID es :", ID)
 else:
     print("")
     print ("Buenas tardes Client ID nº",ID,"soy su asistente personal del Banco MyManueBank")
     
 Costumer = BankAccount (ID)
-print ("De entre los siguientes comandos introduzca la accion que le gustaría realizar:")
-print ("1.Depositar: introduce la cantidad deseada dentro de su cuenta")
-print ("2.Retirar: retire la cantidad indicada de su cuenta")
-print ("3.Comprobar: Compruebe el balance de su cuenta")
-comm = input ("|:")
+while True:
+    print ("De entre los siguientes comandos introduzca la accion que le gustaría realizar:")
+    print ("1.Depositar: introduce la cantidad deseada dentro de su cuenta")
+    print ("2.Retirar: retire la cantidad indicada de su cuenta")
+    print ("3.Comprobar: Compruebe el balance de su cuenta")
+    comm = input ("|:")
 
-while comm not in ["1", "2", "3", "Depositar", "Retirar", "Comprobar"]:
-    print("Comando inválido, por favor introduzca un número o nombre de comando válido.")
-    comm = input("|:")
+    while comm not in ["1", "2", "3", "Depositar", "Retirar", "Comprobar"]:
+        print("Comando inválido, por favor introduzca un número o nombre de comando válido.")
+        comm = input("|:")
 
-if comm == "Depositar" or comm == "1":
-    print ("Introduzca cantidad a depositar")
-    quant = input ("|:")
-    print (Costumer.depositar(quant))
+    if comm == "Depositar" or comm == "1":
+        print ("Introduzca cantidad a depositar")
+        quant = int (input ("|:"))
+        print (Costumer.depositar(quant))
 
-if comm == "Retirar" or comm == "2":
-    print ("introduzca cantidad a retirar")
-    quant = input ("|:")
-    print (Costumer.retirar(quant)) 
+    if comm == "Retirar" or comm == "2":
+        print ("Introduzca cantidad a retirar")
+        quant = int (input ("|:"))
+        print (Costumer.retirar(quant)) 
 
-if comm == "Comprobar" or comm == "3":
-    print(Costumer.cantidad)
+    if comm == "Comprobar" or comm == "3":
+        print(Costumer.cantidad())
+    
+    if comm == "Salir":
+        print ("Que tengaun buen dia")
+        break
