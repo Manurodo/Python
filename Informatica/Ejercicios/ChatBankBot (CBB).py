b72208bbc3d7a384a766c093a16c5bccd1ac9671
Manurodo <manuelrosales2005@outlook.es>
2024-10-31 13:04:20 +0100

    31/10

--- Informatica/Ejercicios/ChatBankBot (CBB).py
+++ Informatica/Ejercicios/ChatBankBot (CBB).py
@@ -4,18 +4,20 @@ print ("Por favor introduzca su MyBank ID")
 print ("Si no tiene un MyBank ID introduzca su nombre y le proporcionaremos un MyBanl ID nuevo")
 
 in_ = input ("|:")
-if int(in_)  ValueError:
+try:
+    int(in_)
+except:
+    ValueError
     name = in_
     ID = randint (11111, 99999)
     print ("Buenas tardes",name,", soy su asistente personal del Banco MyManueBank")
-else:
     ID = in_
 
 
 Costumer = BankAccount (ID)
 
 print ("De entre los siguientes comandos introduzca la accion que le gustaría realizar:")
-print ("Introducir: introduce la cantidad deseada dentro de su cuenta")
-print ("Retirar: retire la cantidad indicada de su cuenta")
-print ("Comprobar: Compruebe el balance de su cuenta")
+print ("1.Introducir: introduce la cantidad deseada dentro de su cuenta")
+print ("2.Retirar: retire la cantidad indicada de su cuenta")
+print ("3.Comprobar: Compruebe el balance de su cuenta")
 comm = input ("|:")
