4e8d1c3a3bf3445612b1c82fac6e796f74b2a425
Manurodo <manuelrosales2005@outlook.es>
2024-11-15 12:06:53 +0100

    15/11

--- Informatica/Ejercicios/boom.py
+++ Informatica/Ejercicios/boom.py
@@ -1,9 +1,9 @@
 import time
 import random
-n = random.randint(0,20)
-while n>-1:
+n = random.randint(0,20) # Creo un int aleatorio
+while n>0: # Mientras ese int no sea >=0 
     print (n)
-    n=n-1
-    time.sleep(1)
+    n=n-1 # Reducir el int por 1
+    time.sleep(1) # Esperar 1 seg para la siguiente iteracion
 print ("¡¡¡¡BOOM!!!!")
 
