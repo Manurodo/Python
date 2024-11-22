ae9a62e5079d4f276c68c53ba7dca67f1bcc8617
Manurodo <manuelrosales2005@outlook.es>
2024-11-22 12:19:55 +0100

    22/11

--- Informatica/Experimentos/tuple.py
+++ Informatica/Experimentos/tuple.py
@@ -1,7 +1,10 @@
-list = ("a", "b", "c")
-result = list [2]
+tuple = ("a", "b", "c")
+result = tuple [2]
 print(result) #c
-x,y,z= list
+x,y,z= tuple
 print(x) #a
-
 print (y) #b
+list = ("a", "b", "c")
+b = "b"
+list ("d")
+print (list)
