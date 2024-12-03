52440c19a12bb79b04281d2f280f60ebc4c34a56
Manurodo <manuelrosales2005@outlook.es>
2024-12-03 08:53:05 +0100

    03/12

--- Informatica/Ejercicios/Chatbot.py
+++ Informatica/Ejercicios/Chatbot.py
@@ -1,32 +1,53 @@
 import random
 from datetime import *
 from Class import fecha
+from sympy import true
 
 
 #Objetivo:Encontrar edad y nombre de una persona
 n_scam = random.randint(20, 99999)
-
-name = input ("Wenas, encantado men. Me llamo Manuel, como te llamas tu? ")
-print ("Encantado tio, como sea, lo siento por preguntar pero es que necesito tu fecha de nacimiento. Ya es tarde y solo he robado datos de ",n_scam, "personas" )
+print (" ")
+print ("Wenas, encantado men. Me llamo Manuel, como te llamas tu? ")
+name = input ("/>")
+print (" ")
+print ("Encantado",name,", como sea, lo siento por preguntar pero es que necesito tu fecha de nacimiento. Ya es tarde y solo he robado datos de ",n_scam, "personas" )
 print ("Cual es tu fecha de nacimiento? AAAA-MM-DD ")
 age = input ("/>")
+print (" ")
+scam = fecha(age)
 
-scam = fecha(name, age)
-
+n = 1
 
 while True:
-    if scam.confirmar_fecha() is None:
-        print("Creo que la fecha que pusiste no existe o no es válida. No pasa nada, todo el mundo se equivoca. Pero como te decía, tengo prisa, así que me puedes decir una fecha válida, por favor.")
 
-    elif scam.confirmar_fecha() is False:
+    if scam.valid(age) is None:
+        print (" ")
+        print("Creo que la fecha que pusiste no existe o no es válida. No pasa nada, todo el mundo se equivoca. Pero como te decía, tengo prisa, así que me puedes decir una fecha válida, por favor.")
+        x = x+1
+
+    elif scam.valid(age) is False:
+        print (" ")
         print("¡Ostia chaval! ¡Un viajero del tiempo! Pero no, en serio, por favor, necesito tu fecha de nacimiento.")
+        x = x+1
 
     elif scam.underage() is True:
+        print (" ")
         print("Ufff, meterme con los datos de un menor puede ponerme en problemas, mejor dejamos la fecha sin registrar")
         break
+    elif x >= 3:
+        print (" ")
+        print ("Ok, im starting to think that you're kidding me, its the police on the line?")
+        print ("Not that I care, im just a chatbot")
+    elif x >= 5:
+        print (" ")
+        print ("oK, im done, bye")
+        break
     else:
+        print (" ")
+        print ("gracias, shau")
         break  
-
-    age = input("Venga, de nuevo, el formato era AAAA-MM-DD, por si acaso tenías dificultades de aprendizaje: ")
+    
+    print (" ")
+    print ("Venga, de nuevo, el formato era AAAA-MM-DD, por si acaso tenías dificultades de aprendizaje: ")
+    age = input ("/>")
     scam.age_cadena = age
-print ("gracias, shau")
